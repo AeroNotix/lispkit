db9e7163a9aef3f41af2e71f074611dae0f5ac39
Aaron France <aaron.l.france@gmail.com>
2014-08-09 17:27:34 +0200

    Put load-url into a function

--- lispkit.lisp
+++ lispkit.lisp
@@ -8,6 +8,10 @@
 
 (defparameter *current-tab* nil)
 
+(defun load-url (url &optional view)
+  (webkit.foreign:webkit-web-view-load-uri
+   (or view *current-tab*) url))
+
 (defun handle-key (window event)
   (declare (ignore window))
   (print event)
@@ -21,8 +25,7 @@
                       (webkit.foreign:webkit-web-view-new))))
       (gtk-container-add window view)
       (gtk-container-add window (make-instance 'gtk-scrolled-window))
-      (webkit.foreign:webkit-web-view-load-uri
-       view "http://www.github.com/AeroNotix/lispkit")
       (g-signal-connect window "key_press_event"
                         #'handle-key)
+      (load-url "http://www.github.com/AeroNotix/lispkit")
       (gtk-widget-show-all window))))
