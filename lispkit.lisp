124d97a3d199cb2eedcd5287eb753e4449eb3ea7
Aaron France <aaron.l.france@gmail.com>
2014-08-10 19:19:13 +0200

    Use a ui file

--- lispkit.lisp
+++ lispkit.lisp
@@ -26,11 +26,12 @@
 (defun main (&rest args)
   (declare (ignore args))
   (within-main-loop
-    (let ((window (make-instance 'gtk:gtk-window :title "lispkit!"))
-          (view (setq *current-tab*
-                      (webkit.foreign:webkit-web-view-new))))
-      (gtk-container-add window view)
-      (gtk-container-add window (make-instance 'gtk-scrolled-window))
+    (let* ((ui     (load-ui-from-file "main.ui"))
+           (window (gtk:gtk-builder-get-object ui "mainwindow"))
+           (frame  (gtk:gtk-builder-get-object ui "webkit-frame"))
+           (view   (setq *current-tab*
+                         (webkit.foreign:webkit-web-view-new))))
+      (gtk-container-add frame view)
       (g-signal-connect window "key_press_event"
                         #'handle-key)
       (load-url "http://www.github.com/AeroNotix/lispkit")
