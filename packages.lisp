65e5b91b1a523fa250f78fb891e4f69e895adfeb
Aaron France <aaron.l.france@gmail.com>
2014-08-12 00:04:29 +0200

    Remove dependency on drakma, why did I have that?

--- packages.lisp
+++ packages.lisp
@@ -1,5 +1,4 @@
 (defpackage lispkit
   (:use :gtk :gdk :gdk-pixbuf :gobject
-        :drakma :cl-webkit
-        :glib :gio :pango :cairo :common-lisp)
+        :cl-webkit :glib :gio :pango :cairo :common-lisp)
   (:export #:main))
