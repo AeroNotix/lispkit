bc563e9a3ca626ca96b92a430f480270d159b22c
Aaron France <aaron.l.france@gmail.com>
2014-08-27 11:44:41 +0200

    Put #'main into #'within-main-loop

--- lispkit.lisp
+++ lispkit.lisp
@@ -16,31 +16,32 @@
       (error (format nil "non existent path: ~s" path))))
 
 (defun main (&optional (destroy? nil))
-  (let* ((ui      (load-ui-from-file
-                   (asdf:system-relative-pathname :lispkit "main.ui")))
-         (window  (gtk:gtk-builder-get-object ui "mainwindow"))
-         (frame   (gtk:gtk-builder-get-object ui "scrolledwindow"))
-         (entry   (gtk:gtk-builder-get-object ui "entry_box"))
-         (view    (make-webview))
-         (nb      (gtk:gtk-builder-get-object ui "webviewcontainer"))
-         (browser (make-browser ui view)))
-    (gtk-notebook-set-show-tabs nb nil)
-    (gtk-container-add frame view)
-    (g-signal-connect window "key_press_event"
-                      (make-key-dispatcher browser))
-    (g-signal-connect nb "switch-page"
-                      (make-page-listener browser))
-    (when destroy?
-      (g-signal-connect window "destroy"
-                        (lambda (widget)
-                          (declare (ignore widget))
-                          (leave-gtk-main))))
-    (load-url *default-page* browser)
-    (gtk-widget-hide entry)
-    ;; TODO - Add error handling to this.
-    (load-rc-file)
-    (dolist (widget (list window frame view))
-      (gtk-widget-show widget))))
+  (within-main-loop
+    (let* ((ui      (load-ui-from-file
+                     (asdf:system-relative-pathname :lispkit "main.ui")))
+           (window  (gtk:gtk-builder-get-object ui "mainwindow"))
+           (frame   (gtk:gtk-builder-get-object ui "scrolledwindow"))
+           (entry   (gtk:gtk-builder-get-object ui "entry_box"))
+           (view    (make-webview))
+           (nb      (gtk:gtk-builder-get-object ui "webviewcontainer"))
+           (browser (make-browser ui view)))
+      (gtk-notebook-set-show-tabs nb nil)
+      (gtk-container-add frame view)
+      (g-signal-connect window "key_press_event"
+                        (make-key-dispatcher browser))
+      (g-signal-connect nb "switch-page"
+                        (make-page-listener browser))
+      (when destroy?
+        (g-signal-connect window "destroy"
+                          (lambda (widget)
+                            (declare (ignore widget))
+                            (leave-gtk-main))))
+      (load-url *default-page* browser)
+      (gtk-widget-hide entry)
+      ;; TODO - Add error handling to this.
+      (load-rc-file)
+      (dolist (widget (list window frame view))
+        (gtk-widget-show widget)))))
 
 (defun do-main (&rest args)
   (declare (ignore args))
