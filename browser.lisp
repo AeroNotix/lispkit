45f1de27782483afab966875d732deeec65c7269
Javier Olaechea <pirata@gmail.com>
2014-09-28 19:50:55 -0500

    If URL doesn't include a scheme add http

--- browser.lisp
+++ browser.lisp
@@ -118,6 +118,7 @@
     (values)))
 
 (defun load-url (url browser)
+  "Load the url in the current webview of the browser."
   (webkit2:webkit-web-view-load-uri (webview browser) url))
 
 (defcommand reload-page (browser)
@@ -143,12 +144,20 @@
   "Move backwards a page."
   (webkit2:webkit-web-view-go-back (webview browser)))
 
+(defun ensure-url-has-scheme (url)
+  "Ensure that the URL is fully qualified, including the scheme (http://,
+  ftp://, etc)."
+  (if (search "://" url)
+      url
+      (format nil "http://~A" url)))
+
 (defcommand browse-url (browser)
   "Browse the the named URL."
   (with-browser-input browser url
-    (if (purl:url-p url)
-        (load-url url browser)
-        (apply-jumps url browser))))
+    (let ((url (ensure-url-has-scheme url)))
+      (if (purl:url-p url)
+          (load-url url browser)
+          (apply-jumps url browser)))))
 
 (defun apply-zoom (browser amount)
   (let* ((wv (webview browser))
