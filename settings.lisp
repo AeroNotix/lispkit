5c74f83584c386065c7bace3fa1113cef8af145c
Florian MARGAINE <florian@margaine.com>
2014-12-05 10:57:07 +0100

    Automatically create the cookie directory.

--- settings.lisp
+++ settings.lisp
@@ -3,3 +3,5 @@
 (defparameter *cookie-path-dir* (merge-pathnames  #P"lispkit/" (get-xdg-config-dir)))
 (defparameter *cookie-type* :webkit-cookie-persistent-storage-text)
 (defparameter *cookie-accept-policy* :webkit-cookie-policy-accept-always)
+
+(ensure-directories-exist *cookie-path-dir*)
