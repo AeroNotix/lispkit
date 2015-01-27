540d7936e42be20688ced1bacd3b796a6aa0e803
Florian MARGAINE <florian@margaine.com>
2015-01-27 19:14:44 +0100

    Adds the keys tests

--- test/commands.lisp
+++ test/commands.lisp
@@ -3,17 +3,17 @@
 
 (5am:in-suite commands)
 
+(lispkit:defcommand test-command (browser)
+  "Test command"
+  (string= browser "fixture"))
+
 (5am:test test-command-p
-  (lispkit:defcommand test-command ()
-    "Test command")
   (multiple-value-bind (value presentp)
       (lispkit::command-p "test-command")
+    (declare (ignore value))
     (5am:is-true (eq presentp t))))
 
 (5am:test test-run-named-command
-  (lispkit:defcommand test-command (browser)
-    "Test command"
-    (string= browser "fixture"))
   (5am:is-true (eq (lispkit::run-named-command "test-command" "fixture") t)))
 
 (5am:test test-load-rc-file
@@ -26,5 +26,6 @@
   (open (merge-pathnames (user-homedir-pathname) #p".lispkitrc") :direction :probe :if-does-not-exist :create)
   (multiple-value-bind (value presentp)
       (gethash "reload-config" lispkit::*available-commands*)
+    (declare (ignore value))
     (5am:is-true (eq presentp t))
     (5am:is-true (eq (lispkit::reload-config nil) t))))
--- test/keys.lisp
+++ test/keys.lisp
@@ -2,3 +2,30 @@
 
 
 (5am:in-suite keys)
+
+(5am:test test-make-keymap
+  (let ((keymap (lispkit::make-keymap)))
+    (5am:is-true (eq 'lispkit::keymap (type-of keymap)))
+    (5am:is-true (eq 'hash-table (type-of (lispkit::bindings keymap))))))
+
+(5am:test test-keymap->keydescs
+  "Tests keymap->keydesc* and keymap->keydesc"
+  (let ((keybind (lispkit::keymap->keydesc* nil "foo" nil)))
+    (5am:is-true (eq 'lispkit::keybind (type-of keybind)))
+    (5am:is-true (string= "foo" (lispkit::key keybind))))
+  (let ((keymap (make-instance 'lispkit::keymap)))
+    (5am:is-true (string= "foo foo"
+			  (lispkit::key
+			   (first
+			    (let ((keymap (make-instance 'lispkit::keymap)))
+			      (setf (gethash
+				     "foo"
+				     (lispkit::bindings keymap))
+				    "bar")
+			      (lispkit::keymap->keydesc* nil "foo" keymap))))))))
+
+(5am:test test-define-key
+  (let ((map (make-instance 'lispkit::keymap)))
+    (lispkit::define-key map "foo" "bar")
+    (5am:is-true (string= "bar"
+			  (gethash "foo" (lispkit::bindings map))))))
