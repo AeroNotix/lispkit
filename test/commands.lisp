61a7a847c322f9caf6e909e32b63a89a45a6c947
Florian MARGAINE <florian@margaine.com>
2015-01-27 10:39:15 +0100

    Adds remaining tests for commands

--- test/commands.lisp
+++ test/commands.lisp
@@ -15,3 +15,12 @@
     "Test command"
     (eq browser "fixture"))
   (5am:is-true (eq (lispkit::run-named-command "test-command" "fixture") t)))
+
+(5am:test test-load-rc-file
+  (5am:is-true (eq (lispkit::load-rc-file) t)))
+
+(5am:test test-reload-config
+  (multiple-value-bind (value presentp)
+      (gethash "reload-config" lispkit::*available-commands*)
+    (5am:is-true (eq presentp t))
+    (5am:is-true (eq (lispkit::reload-config nil) t))))
