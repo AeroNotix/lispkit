325c34ffe76a230ebb9bc35479f1a69eef0d68ad
Florian MARGAINE <florian@margaine.com>
2015-01-27 14:23:06 +0100

    Fixes tests working locally, but not remotely
    Well, the s/eq/string= could work remotely too, but it's
    platform-dependent.

--- test/commands.lisp
+++ test/commands.lisp
@@ -13,13 +13,17 @@
 (5am:test test-run-named-command
   (lispkit:defcommand test-command (browser)
     "Test command"
-    (eq browser "fixture"))
+    (string= browser "fixture"))
   (5am:is-true (eq (lispkit::run-named-command "test-command" "fixture") t)))
 
 (5am:test test-load-rc-file
+  ;; Create the ~/.lispkitrc file
+  (open (merge-pathnames (user-homedir-pathname) #p".lispkitrc") :direction :probe :if-does-not-exist :create)
   (5am:is-true (eq (lispkit::load-rc-file) t)))
 
 (5am:test test-reload-config
+  ;; Create the ~/.lispkitrc file
+  (open (merge-pathnames (user-homedir-pathname) #p".lispkitrc") :direction :probe :if-does-not-exist :create)
   (multiple-value-bind (value presentp)
       (gethash "reload-config" lispkit::*available-commands*)
     (5am:is-true (eq presentp t))
